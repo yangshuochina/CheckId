bb92cd566aa83bc5efe8fd266c434caf967c3c8d
Shuo Yang <yangshuochina@gmail.com>
2022-09-29 13:14:59 +0800

    feat:Initial Commit.

--- CheckId/Form1.Designer.cs
+++ CheckId/Form1.Designer.cs
@@ -28,13 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbIdNum = new System.Windows.Forms.TextBox();
+            this.tbResultNum = new System.Windows.Forms.TextBox();
+            this.btnCalcNum = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "原始号码：";
+            // 
+            // tbIdNum
+            // 
+            this.tbIdNum.Location = new System.Drawing.Point(102, 19);
+            this.tbIdNum.Name = "tbIdNum";
+            this.tbIdNum.Size = new System.Drawing.Size(346, 21);
+            this.tbIdNum.TabIndex = 1;
+            // 
+            // tbResultNum
+            // 
+            this.tbResultNum.Location = new System.Drawing.Point(102, 56);
+            this.tbResultNum.Multiline = true;
+            this.tbResultNum.Name = "tbResultNum";
+            this.tbResultNum.ReadOnly = true;
+            this.tbResultNum.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbResultNum.Size = new System.Drawing.Size(452, 457);
+            this.tbResultNum.TabIndex = 2;
+            // 
+            // btnCalcNum
+            // 
+            this.btnCalcNum.Location = new System.Drawing.Point(479, 16);
+            this.btnCalcNum.Name = "btnCalcNum";
+            this.btnCalcNum.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcNum.TabIndex = 3;
+            this.btnCalcNum.Text = "计算号码";
+            this.btnCalcNum.UseVisualStyleBackColor = true;
+            this.btnCalcNum.Click += new System.EventHandler(this.btnCalcNum_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "计算号码：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(561, 525);
+            this.Controls.Add(this.btnCalcNum);
+            this.Controls.Add(this.tbResultNum);
+            this.Controls.Add(this.tbIdNum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CheckId";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbIdNum;
+        private System.Windows.Forms.TextBox tbResultNum;
+        private System.Windows.Forms.Button btnCalcNum;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- CheckId/Form1.cs
+++ CheckId/Form1.cs
@@ -16,5 +16,116 @@ namespace CheckId
         {
             InitializeComponent();
         }
+
+        String m_strOriginNum = "";
+        String m_strValidatedNum = "";
+    
+        public readonly int[] m_weight = { 7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2 };    //十七位数字本体码权重
+        public readonly char[] m_validate = { '1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2' };    //mod11,对应校验码字符值    
+        
+        private char[] m_NumToValidate;
+        private void btnCalcNum_Click(object sender, EventArgs e)
+        {
+            int i, j;
+            String strTemp;
+            m_strOriginNum = tbIdNum.Text;
+            //MessageBox.Show(m_strOriginIdNum);
+            m_NumToValidate = new char[18];
+
+            if (CheckInputNum(m_strOriginNum) == false)
+            {
+                MessageBox.Show("号码长度必须为18位！(仅第11到14位可以忽略。)", "Error");
+            }
+            else
+            {
+                //输入数字符合要求，开始校验所有可能的号码
+
+                tbResultNum.Text = ""; 
+                for (i=1; i <= 12; i++)
+                {
+                    for (j=1; j<=31; j++)
+                    {
+                        if ((i == 4 || i == 6 || i == 9 || i == 11) && j == 31) break;
+                        if ((i == 2) && (j >= 30)) break;
+
+                        // 开始有效日期验证
+                        m_NumToValidate[10] = (char)(i / 10 + (byte)'0');
+                        m_NumToValidate[11] = (char)(i % 10 + (byte)'0');
+                        m_NumToValidate[12] = (char)(j / 10 + (byte)'0');
+                        m_NumToValidate[13] = (char)(j % 10 + (byte)'0');
+                        if (true == CheckValidateCode(m_NumToValidate))
+                        {
+                            strTemp = new string(m_NumToValidate);
+                            tbResultNum.Text += strTemp  + "\r\n";
+                        }
+
+                    }
+                }
+            }
+        }
+        private bool CheckValidateCode(char[] numChar)
+        {
+            byte[]  num = new byte[18];
+            char ch;
+            int i;
+            int sum = 0;
+            int mode = 0;
+            for (i=0; i < 18; i++)
+            {
+                ch = numChar[i];
+                if ((i <= 16) || (i == 17 && ch != 'X'))
+                {
+                    num[i] = (byte)(ch - '0');
+                } 
+                else
+                {
+                    num[i] = 10;
+                }
+            }
+            // 计算加权和以及余数
+            for (i=0; i < 17; i++)
+            {
+                sum += num[i] * m_weight[i];
+            }
+            mode = sum % 11;
+
+            if (mode == num[17])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool CheckInputNum(String strNum)
+        {
+            char c;
+            bool result = true;
+
+            if (strNum.Length != 18)
+            {
+                result = false;
+            }
+            for (int i=0; i<18; i++)
+            {
+                c = strNum[i];
+                if ((c >= '0' && c <= '9') ||
+                    (i >= 10 && i <= 13) ||
+                    (i==17 && c =='X'))
+                {
+                    m_NumToValidate[i] = c;
+                    continue;
+                }
+                else
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
+        }
     }
 }
